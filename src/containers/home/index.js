5294679377d76a142ea34178ff1e747425484914
simon2828 <simon.tanner28@gmail.com>
2017-10-21 09:55:44 +0100

    added redux

---
+++ src/containers/home/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+const Home = props => (
+  <div>
+    <h1>Home</h1>
+
+  </div>
+)
+
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changePage: () => push('/about-us')
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
